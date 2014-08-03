ab6f41942f6d9ed61c7a155a7a63ed646488542a
Alex Wolfe <thewolfe@gmail.com>
2014-08-02 21:53:24 -0700

    more gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp            = require('gulp');
+var gulpBowerFiles  = require('gulp-bower-files');
 var sass            = require('gulp-sass');
 var autoprefixer    = require('gulp-autoprefixer');
 var minifyCSS       = require('gulp-minify-css');
+var concat          = require('gulp-concat');
+var webpack         = require('gulp-webpack');
+var uglify          = require('uglify-js');
 var rev             = require('gulp-rev');
 var imageResize     = require('gulp-image-resize');
 var connect         = require('gulp-connect');
@@ -83,6 +87,20 @@ gulp.task('images:production', function() {
 
 
 /**
+*  JAVASCRIPT PREPROCESSING
+*
+*  bower file, common js modules, uglify, minify
+*/
+gulp.task("bower-files", function(){
+    gulpBowerFiles().pipe(gulp.dest("./public/js/libs"));
+});
+
+gulp.task('js', ['bower-files'], function() {
+
+});
+
+
+/**
 *  WATCH
 *
 *  Rerun process after any of these files are edited
@@ -90,6 +108,7 @@ gulp.task('images:production', function() {
 gulp.task('watch', function() {
   gulp.watch('resources/scss/**/*.scss', ['css', 'harp']);
   gulp.watch('resources/images/**/*.{jpg,png,gif}', ['images', 'harp']);
+  gulp.watch('public/**/*.jade', ['harp']);
 });
 
 
@@ -113,5 +132,5 @@ gulp.task('connect', function() {
 *
 *  Local and production build tasks
 */
-gulp.task('default', ['css', 'images', 'harp', 'watch', 'connect']);
-gulp.task('production', ['css:production', 'images', 'harp']);
+gulp.task('default', ['css', 'images', 'js', 'harp', 'watch', 'connect']);
+gulp.task('production', ['css:production', 'images:production', 'js', 'harp']);
